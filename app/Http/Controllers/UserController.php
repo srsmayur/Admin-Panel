62424261480ca45966c98a7ce07e20da84aa4cae
Mayur Talsania <srs.mayur@gmail.com>
2018-03-16 18:54:59 +0530

    Add Profile Panel and Edit panel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 3/16/2018
+ * Time: 10:43 AM
+ */
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 3/16/2018
+ * Time: 4:52 PM
+ */
